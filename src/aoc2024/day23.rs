b2e3cfb3d5df56f7ea33f2f6df6c2347fa396a5b
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2025-02-25 23:31:14 +0000

    WIP graph

--- src/aoc2024/day23.rs
+++ src/aoc2024/day23.rs
@@ -1,5 +1,13 @@
 // https://adventofcode.com/2024/day/23
 
+use std::collections::HashMap;
+
+use petgraph::{
+    algo::kosaraju_scc,
+    dot::Dot,
+    graph::{NodeIndex, UnGraph},
+};
+
 use crate::utils::get_lines;
 
 struct Input {
@@ -19,13 +27,50 @@ fn parse_input(input_file: &str) -> Input {
     Input { conn_pairs }
 }
 
+fn build_conn_graph(
+    conn_pairs: &Vec<(String, String)>,
+) -> (UnGraph<String, ()>, HashMap<String, NodeIndex>) {
+    let mut graph = UnGraph::<String, ()>::new_undirected();
+    let mut node_indices = HashMap::<String, NodeIndex>::new();
+
+    // Add nodes and edges
+
+    // First create all the nodes
+    for (node1, node2) in conn_pairs {
+        if !node_indices.contains_key(node1) {
+            let index = graph.add_node(node1.clone());
+            node_indices.insert(node1.clone(), index);
+        }
+
+        if !node_indices.contains_key(node2) {
+            let index = graph.add_node(node2.clone());
+            node_indices.insert(node2.clone(), index);
+        }
+    }
+
+    // Then add all edges in a separate pass
+    for (node1, node2) in conn_pairs {
+        let node1_index = node_indices.get(node1).unwrap();
+        let node2_index = node_indices.get(node2).unwrap();
+        graph.add_edge(*node1_index, *node2_index, ());
+    }
+
+    (graph, node_indices)
+}
+
 pub fn get_num_conn_start_t(input_file: &str) -> u32 {
     let input = parse_input(input_file);
 
-    for conn_pair in input.conn_pairs {
-        println!("{:?}", conn_pair);
+    let (graph, node_indices) = build_conn_graph(&input.conn_pairs);
+
+    println!("graph: {:?}", Dot::new(&graph));
+
+    let conn_comps = kosaraju_scc(&graph);
+
+    for conn_comp in conn_comps {
+        println!("conn_comp: {:?}", conn_comp);
     }
-    
+
     0
 }
 
