8b38cbd67fbc885ce952f4d56b0b9d164dfa6bfe
Andrew McMahon <andrew.p.mcmahon@gmail.com>
2025-08-22 17:25:33 +0100

    Skeleton for tests done

--- src/aoc2019/day05.rs
+++ src/aoc2019/day05.rs
@@ -39,10 +39,135 @@ mod tests {
     }
 
     #[test]
-    fn test_run_diagnostic_code() {
+    fn test_run_diagnostic_code_part_one() {
         let mut input_intcode = parse_intcode_input("input/2019/day05.txt");
         let mut outputs = vec![];
         run_intcode(&mut input_intcode, 0, Some(1), &mut outputs);
         assert_eq!(*outputs.last().unwrap(), 4601506);
+    }
+
+    #[test]
+    fn test_run_intcode_test04() {
+        let expected_intcode_eq = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8];
+        let expected_intcode_neq = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test04.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_eq,
+            run_intcode(&mut input_intcode, 0, Some(8), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_neq,
+            run_intcode(&mut input_intcode, 0, Some(7), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_intcode_test05() {
+        let expected_intcode_lt = vec![3,9,7,9,10,9,4,9,99,1,8];
+        let expected_intcode_gt = vec![3,9,7,9,10,9,4,9,99,0,8];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test05.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_lt,
+            run_intcode(&mut input_intcode, 0, Some(7), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_gt,
+            run_intcode(&mut input_intcode, 0, Some(9), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_intcode_test06() {
+        let expected_intcode_eq = vec![3,3,1108,1,8,3,4,3,99];
+        let expected_intcode_neq = vec![3,3,1108,0,8,3,4,3,99];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test06.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_eq,
+            run_intcode(&mut input_intcode, 0, Some(8), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_neq,
+            run_intcode(&mut input_intcode, 0, Some(7), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_intcode_test07() {
+        let expected_intcode_lt = vec![3,3,1107,1,8,3,4,3,99];
+        let expected_intcode_gt = vec![3,3,1107,0,8,3,4,3,99];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test07.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_lt,
+            run_intcode(&mut input_intcode, 0, Some(7), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_gt,
+            run_intcode(&mut input_intcode, 0, Some(9), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_intcode_test08() {
+        let expected_intcode_zero = vec![3,12,6,12,15,1,13,14,13,4,13,99,0,0,1,9];
+        let expected_intcode_nonzero = vec![3,12,6,12,15,1,13,14,13,4,13,99,1,0,1,9];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test08.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_zero,
+            run_intcode(&mut input_intcode, 0, Some(0), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_nonzero,
+            run_intcode(&mut input_intcode, 0, Some(1), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_intcode_test09() {
+        let expected_intcode_zero = vec![3,3,1105,1,9,1101,0,0,12,4,12,99,1];
+        let expected_intcode_nonzero = vec![3,3,1105,0,9,1101,0,0,12,4,12,99,1];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test09.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_zero,
+            run_intcode(&mut input_intcode, 0, Some(0), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_nonzero,
+            run_intcode(&mut input_intcode, 0, Some(1), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_intcode_test10() {
+        let expected_intcode_lt = vec![3,3,1105,1,9,1101,0,0,12,4,12,99,1];
+        let expected_intcode_eq = vec![3,3,1105,0,9,1101,0,0,12,4,12,99,1];
+        let expected_intcode_gt = vec![3,3,1105,0,9,1101,0,0,12,4,12,99,1];
+        let mut input_intcode = parse_intcode_input("input/2019/day05_test10.txt");
+        let mut outputs = vec![];
+        assert_eq!(
+            expected_intcode_lt,
+            run_intcode(&mut input_intcode, 0, Some(7), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_eq,
+            run_intcode(&mut input_intcode, 0, Some(8), &mut outputs)
+        );
+        assert_eq!(
+            expected_intcode_gt,
+            run_intcode(&mut input_intcode, 0, Some(9), &mut outputs)
+        );
+    }
+
+    #[test]
+    fn test_run_diagnostic_code_part_two() {
+        let mut input_intcode = parse_intcode_input("input/2019/day05.txt");
+        let mut outputs = vec![];
+        run_intcode(&mut input_intcode, 0, Some(5), &mut outputs);
+        assert_eq!(*outputs.last().unwrap(), 0);
     }
 }
